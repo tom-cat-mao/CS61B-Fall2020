303dcf3adb8c33be36052e4520d9e46cd5fac89b
Tom Cat <maoy896@gmail.com>
2024-06-10 16:32:59 +0800

    the whole JUnit in circle double list

--- lecture_exercise/Lecture-5/JUnit.java
+++ lecture_exercise/Lecture-5/JUnit.java
@@ -1,36 +1,73 @@
-public class JUnit {
-    private IntList sentinel;
+public class JUnit<T> {
+    private IntList<T> sentinel;
     private int size;
 
-    private class IntList {
-        private int x;
-        private IntList next;
-        private IntList prev;
+    private class IntList<T> {
+        T x;
+        IntList<T> next;
+        IntList<T> prev;
     
-        public IntList(int x, IntList next, IntList prev) {
+        public IntList(T x, IntList next, IntList prev) {
             this.x = x;
             this.prev = prev;
             this.next = next;
         }
-    
-        public int getFirst() {
-            return this.x;
-        } 
-    
-        public int getLast() {
-            return this.prev.x;
-        }
+
     }
     
     public JUnit() {
-        this.sentinel.x = 114514;
-        this.sentinel.prev = null;
-        this.sentinel.next = null;
+        this.sentinel = new IntList(114514, null, null);
+        this.size = 0;
     }
 
-    public JUnit(int x) {
-        this.sentinel.x = 114514;
+    public JUnit(T x) {
+        this.sentinel = new IntList(114514, null, null);
         this.sentinel.next = new IntList(x, sentinel, sentinel);
         this.sentinel.prev = sentinel.next;
+        size = 1;
+    }
+
+    public void addFirst(T x) {
+        IntList<T> temp = new IntList(x, sentinel.next, sentinel);
+        temp.next.prev = temp;
+        sentinel.next = temp;
+    }
+
+    public void addLast(T x) {
+        IntList<T> temp = new IntList(x, sentinel, sentinel.prev);
+        sentinel.prev = temp;
+        temp.prev.next = temp;
+    }
+
+    public T getFirst() {
+        return sentinel.next.x;
+    }
+
+    public T getLast() {
+        return sentinel.prev.x;
+    }
+
+    public T popFirst() {
+
+        if (size == 0) {
+            return null;
+        }
+
+        T temp = sentinel.next.x;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        return temp;
+    }
+
+    public T popLast() {
+
+        if (size == 0) {
+            return null;
+        }
+
+        T temp = sentinel.prev.x;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        return temp;
     }
 }
