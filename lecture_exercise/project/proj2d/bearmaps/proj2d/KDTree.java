0cf5eced38121b296a5b6a14bb7280aea0b53dc9
Tom_Cat <maoy896@gmail.com>
2024-11-14 17:36:38 +0800

    Refactor KDTree: streamline node structure and enhance point insertion logic

--- lecture_exercise/project/proj2d/bearmaps/proj2d/KDTree.java
+++ lecture_exercise/project/proj2d/bearmaps/proj2d/KDTree.java
@@ -5,7 +5,7 @@ import bearmaps.proj2ab.PointSet;
 import java.util.List;
 
 public class KDTree implements PointSet {
-    private Node root;
+    private Node root; // the root of the KDTree
 
     /**
      * Instantiates a new KDTree with a list of points.
@@ -14,7 +14,11 @@ public class KDTree implements PointSet {
      * @param lat The latitude of the point to find the nearest neighbor of.
      */
     public KDTree(List<Point> points, double lon, double lat) {
-        root = new Node(new Point(lon, lat), true);
+        root = new Node(new Point(lon, lat));
+
+        for (Point point : points) {
+            insert(root, point);
+        }
     }
 
     /**
@@ -24,17 +28,56 @@ public class KDTree implements PointSet {
         private Point point;
         private Node left;
         private Node right;
-        private boolean isVertical;
 
-        public Node(Point point, boolean isVertical) {
+        public Node(Point point) {
             this.point = point;
-            this.isVertical = isVertical;
+        }
+
+        public double getX() {
+            return point.getX();
+        }
+
+        public double getY() {
+            return point.getY();
         }
     }
 
-    private void insert(Node node, boolean isVertical) {
-        return;
+    /**
+     * Inserts a point into the KDTree.
+     * @param root The root of the KDTree to insert the point into.
+     * @param point The point to insert.
+     */
+    public void insert(Node root, Point point) {
+        insert_p(root, point, true);
     }
+
+    /**
+     * private method to insert a point into the KDTree.
+     * @param root The root of the KDTree to insert the point into.
+     * @param point The point to insert.
+     * @param isVertical A boolean indicating whether the node is vertical.
+     * @return The root of the KDTree with the point inserted.
+     */
+    private Node insert_p(Node root, Point point, boolean isVertical) {
+        if (root == null) {
+            return new Node(point);
+        }
+        if (isVertical) {
+            if (point.getX() < root.getX()) {
+                root.left = insert_p(root.left, point, !isVertical);
+            } else {
+                root.right = insert_p(root.right, point, !isVertical);
+            }
+        } else {
+            if (point.getY() < root.getY()) {
+                root.left = insert_p(root.left, point, !isVertical);
+            } else {
+                root.right = insert_p(root.right, point, !isVertical);
+            }
+        }
+        return root;
+    }
+
 
     /**
      * Returns the point in this set closest to the given point (x, y).
@@ -43,6 +86,15 @@ public class KDTree implements PointSet {
      * @return The point in this set closest to (x, y).
      */
     public Point nearest(double x, double y) {
+        return null;
+    }
+
+    /**
+     * private method to find the nearest point in the KDTree.
+     * @param best
+     * @return The nearest point in the KDTree.
+     */
+    private Point nearest_p(Point best) {
         return null;
     }
 }
